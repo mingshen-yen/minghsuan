69c0404e86078d55ff5ae2afcc30dc28bf52f093
mingshen-yen <minghsuanyen@gmail.com>
2026-05-19 12:07:42 +0200

    bg color

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -27,7 +27,7 @@ export const Header = () => {
                   key={to}
                   to={to}
                   className={`flex items-center px-4 transition cursor-pointer ${
-                    active ? "text-amber-300" : "hover:text-slate-400"
+                    active ? "text-[#5de8b0]" : "hover:text-slate-400"
                   }`}
                 >
                   {Icon && <Icon className="h-4 w-4 mr-2" />}
@@ -48,13 +48,13 @@ export const Header = () => {
 
           {/* Mobile Drawer */}
           <div
-            className={`fixed top-0 left-0 z-130 w-full max-h-1200 bg-slate-600 shadow-lg flex flex-col justify-center items-center pb-10 overflow-hidden md:hidden transform transition-transform duration-300 ease-in-out
+            className={`fixed top-0 left-0 z-130 w-full max-h-1200 bg-[rgba(14,22,40)] shadow-lg flex flex-col justify-center items-center pb-10 overflow-hidden md:hidden transform transition-transform duration-300 ease-in-out
   ${open ? "translate-y-0" : "-translate-y-full"}`}
           >
             {/* X Button inside drawer */}
             <button
               onClick={() => setOpen(false)}
-              className="self-end m-4 p-2 rounded-full hover:bg-purple-dark"
+              className="self-end m-4 p-2 rounded-full hover:bg-[rgba(14,22,40,0.85)]"
             >
               <X size={24} />
             </button>
@@ -64,7 +64,7 @@ export const Header = () => {
                 key={to}
                 to={to}
                 onClick={() => setOpen(false)}
-                className="flex items-center px-8 py-4 hover:text-amber-300"
+                className="flex items-center px-8 py-4 hover:text-[#5de8b0]"
               >
                 <Icon className="h-4 w-4 mr-2" />
                 {label}
