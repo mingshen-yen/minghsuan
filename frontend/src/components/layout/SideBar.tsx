f4e6d106680f229fb308736220c9f8821b7f243f
mingshen-yen <minghsuanyen@gmail.com>
2026-05-21 00:39:34 +0200

    social items in right sidebar

--- frontend/src/components/layout/SideBar.tsx
+++ frontend/src/components/layout/SideBar.tsx
@@ -1,21 +1,25 @@
 import { Mail } from "lucide-react";
 import { Link } from "react-router";
 
-export const SocialBar = () => {
-  const socailItems = [
+export const SideBar = () => {
+  const socialItems = [
     {
       to: "https://www.linkedin.com/in/ming-hsuan-yen/",
       label: "LinkedIn",
       icon: Mail,
     },
     { to: "https://github.com/mingshen-yen", label: "GitHub", icon: Mail },
-    { to: "www.youtube.com/@mingshenworld", label: "YouTube", icon: Mail },
+    {
+      to: "http://www.youtube.com/@mingshenworld",
+      label: "YouTube",
+      icon: Mail,
+    },
   ];
   return (
     <>
-      <div className="social_bar">
-        <ul>
-          {socailItems.map(({ to, label, icon: Icon }) => {
+      <div className="right_bar">
+        <ul className="social_items">
+          {socialItems.map(({ to, label, icon: Icon }) => {
             return (
               <Link key={to} to={to} target="_blank" rel="noopener noreferrer">
                 {/* {Icon && <Icon className="h-4 w-4 mr-2" />} */}
