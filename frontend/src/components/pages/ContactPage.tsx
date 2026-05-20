4850806f2771581f8a532f841e4afa80e36be4b5
mingshen-yen <minghsuanyen@gmail.com>
2026-05-20 15:40:25 +0200

    fix the container

--- frontend/src/components/pages/ContactPage.tsx
+++ frontend/src/components/pages/ContactPage.tsx
@@ -6,11 +6,11 @@ import { ContactForm } from "../layout/ContactForm";
 export const ContactPage = () => {
   return (
     <>
-      <div className="container relative z-10">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 items-baseline">
+      <div className="container">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 items-baseline justify-center">
           <div className="flex flex-col gap-8">
             <h2>Reach out me</h2>
-            <div className="flex px-2 gap-6 justify-start items-center">
+            <div className="flex px-2 gap-6 justify-center lg:justify-start items-center">
               <Link to={"mailto:minghsuanyen@gmail.com"}>
                 <Mail className="w-6 h-6" />
               </Link>
@@ -22,8 +22,12 @@ export const ContactPage = () => {
                 <img src="/src/assets/linkedin.svg" className="w-6 h-6" />
               </a>
             </div>
+            <p>
+              You can also send me an email. <br /> I will be in contact with
+              you as soon as I can.
+            </p>
           </div>
-          <div className="col-span-2 p-12 gap-8 flex flex-wrap bg-[#348564] rounded-sm">
+          <div className="md:w-fit p-12 gap-8 flex flex-wrap bg-[#348564] rounded-sm">
             <h3>CONTACT FORM</h3>
             <ContactForm />
           </div>
