16ea9f79e9c45d323b1572f158fec6202cedbf56
mingshen-yen <minghsuanyen@gmail.com>
2026-05-20 15:40:16 +0200

    add socialbar

---
+++ frontend/src/components/layout/SocialBar.tsx
@@ -0,0 +1,30 @@
+import { Mail } from "lucide-react";
+import { Link } from "react-router";
+
+export const SocialBar = () => {
+  const socailItems = [
+    {
+      to: "https://www.linkedin.com/in/ming-hsuan-yen/",
+      label: "LinkedIn",
+      icon: Mail,
+    },
+    { to: "https://github.com/mingshen-yen", label: "GitHub", icon: Mail },
+    { to: "www.youtube.com/@mingshenworld", label: "YouTube", icon: Mail },
+  ];
+  return (
+    <>
+      <div className="social_bar">
+        <ul>
+          {socailItems.map(({ to, label, icon: Icon }) => {
+            return (
+              <Link key={to} to={to} target="_blank" rel="noopener noreferrer">
+                {/* {Icon && <Icon className="h-4 w-4 mr-2" />} */}
+                <li className="social_btn">{label}</li>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
